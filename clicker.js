4461b6db8f5c264bd81e4afa392abc2a3914005b
k-shir0 <kakishiro2190394@gmail.com>
2024-01-29 20:14:20 +0900

    fix: RTの数を押しても反応するように修正

--- clicker.js
+++ clicker.js
@@ -1,10 +1,8 @@
-console.log('clicker.js loaded');
 document.addEventListener('click', function (e) {
     setTimeout(function () {
-            if (e.target.classList.contains('js-icon-retweet')) {
-                var retweetButton = document.querySelector('button[data-action="retweet"]');
+            if (e.target.classList.contains('js-icon-retweet') || e.target.classList.contains('icon-retweet-toggle')) {
+                const retweetButton = document.querySelector('button[data-action="retweet"]');
                 if (retweetButton) {
-                    console.log('true')
                     retweetButton.click();
                 }
             }
